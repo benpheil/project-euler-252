1a3656a26a39dcac0e0bb0337b8786956d861925
Benjamin Pheil <bpheil@irobot.com>
2015-11-07 21:07:07 -0500

    Plot the points for k = 20.

---
+++ p252.py
@@ -0,0 +1,60 @@
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import sys
+
+def TGenerator(kmax):
+    seed = 290797
+    idx = 0
+    while idx <= kmax:
+        idx += 1
+        seed = (seed ** 2) % 50515093
+        yield (seed % 2000) - 1000
+
+def pointGenerator(kmax):
+    T = TGenerator(2*kmax)
+    for T1 in T:
+        T2 = T.next()
+        yield [T1, T2]
+
+class GraphPlotter(object):
+    def __init__(self):
+        # Enable antialiasing for prettier plots
+        pg.setConfigOptions(antialias=True)
+
+        self._w = pg.GraphicsWindow()
+        self._w.setWindowTitle('Problem 252 Graph')
+        self._v = self._w.addViewBox()
+        self._v.setAspectLocked()
+
+        self._g = pg.GraphItem()
+        self._v.addItem(self._g)
+
+    def update(self, points, adjacency):
+        symbols = ['o'] * len(points)
+        self._g.setData(pos=points, adj=adjacency, size=10, symbols=symbols, pxMode=False)
+
+def test():
+    # First 3 points are given in problem.
+    p = pointGenerator(3)
+    assert(p.next() == [527, 144])
+    assert(p.next() == [-488, 732])
+    assert(p.next() == [-454, -947])
+
+def main():
+    kmax = 20
+
+    points = np.array(list(pointGenerator(kmax)))
+    for p in points:
+        print(p)
+    adjacency = np.array([0,0])
+
+    plotter = GraphPlotter()
+    plotter.update(points, adjacency)
+
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
+
+if __name__ == '__main__':
+    test()
+    main()
